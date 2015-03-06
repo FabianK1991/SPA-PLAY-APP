6cb81508e78aa65f108ec9ec2bc9a55b6f200764
ChristianS1991 <christian.schreckenberger@gmail.com>
2015-03-06 05:38:03 +0100

    Testcases
    Not complete some were ambigious

--- test/SPATest.java
+++ test/SPATest.java
@@ -1,10 +1,20 @@
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import models.spa.api.ProcessInstance;
 import models.spa.api.ProcessModel;
 import models.spa.example.MailProcess;
+import models.spa.api.process.buildingblock.*;
+import models.spa.api.process.buildingblock.instance.*;
 
 import org.junit.Test;
+
+
 
 
 public class SPATest {
@@ -25,7 +35,8 @@ public class SPATest {
 		}
 	}
 
-	// ProcessModel
+	// ProcessModel 
+	//done
 	@Test
 	public void getProcessModelById() {
 		// Process.getProcess
@@ -45,7 +56,7 @@ public class SPATest {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			fail("Error on retrieving the Process Model by an ID");
+			fail("Error in retrieving the Process Model by an ID");
 		}
 		
 		try {
@@ -56,10 +67,24 @@ public class SPATest {
 		}
 	}
 	
+	//done bzw kleines todo?
 	@Test
 	public void getProcessIDsKeyword() {
 		// Process.getProcessIDsKeyword
 		ProcessModel pm = createTestModel();
+		HashSet<String> testKeywords = new HashSet<String>();
+		
+		for(int i = 0;  i < (int)(3 + Math.random()*5); i++){
+			String word = "";
+			for(int j = 0; j < (int)(5 + Math.random()*4); j++){
+				char c = (char) (97 + (int)(Math.random() * 26));
+				word = word + c;
+			}
+			testKeywords.add(word);
+		}
+		
+		
+		pm.setKeywords(testKeywords);
 		
 		try {
 			pm.store();
@@ -67,8 +92,48 @@ public class SPATest {
 			e.printStackTrace();
 			fail("Error in store!");
 		}
+		boolean flagContains = false;
+		try {
+			for(ProcessModel newPM:ProcessModel.searchByKeywords(testKeywords)){
+				if (newPM.getId().equals(pm.getId())){
+					flagContains = true;
+				}
+			}
+		} catch (Exception e1) {
+			fail("Process could not be received with the given keywords");
+			e1.printStackTrace();
+		}
+		if(!flagContains){
+			fail("Process was not found when searching by keywords, using every keyword of the process");
+		}
 		
-		fail("Not yet implemented!");
+		HashSet<String> someKeywords = new HashSet<String>();
+		int count = 0;
+		for(String keyword:testKeywords){
+			someKeywords.add(keyword);
+			count ++;
+			
+			if(count == testKeywords.size()-2) break;
+		}
+		
+		flagContains = false;
+		try {
+			for(ProcessModel newPM:ProcessModel.searchByKeywords(someKeywords)){
+				if (newPM.getId().equals(pm.getId())){
+					flagContains = true;
+				}
+			}
+		} catch (Exception e1) {
+			fail("Process could not be received with the given keywords");
+			e1.printStackTrace();
+		}
+		if(!flagContains){
+			fail("Process was not found when searching by keywords, using every keyword of the process");
+		}
+		
+		/*
+		 * ggf noch case für abfrage on keywords?
+		 */
 		
 		try {
 			pm.delete();
@@ -78,10 +143,12 @@ public class SPATest {
 		}
 	}
 	
+	//done
 	@Test
 	public void getProcessIDsBO() {
 		// Process.getProcessIDsBO
 		ProcessModel pm = createTestModel();
+		Set<BusinessObject> bos = pm.getBusinessObjects();
 		
 		try {
 			pm.store();
@@ -89,8 +156,26 @@ public class SPATest {
 			e.printStackTrace();
 			fail("Error in store!");
 		}
-		
-		fail("Not yet implemented!");
+		HashSet<String> boUris = new HashSet<String>();
+		for(BusinessObject bo: bos){
+			boUris.add(bo.getId());
+		}
+		List<ProcessModel> processModels = null;
+		try {
+			ProcessModel.searchByBusinessObjects(boUris);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			fail("Error while searching for process model with business object(s)");
+		}
+		boolean flagContains = false;
+		for(ProcessModel newModel:processModels){
+			if(newModel.getId().equals(pm.getId())){
+				flagContains = true;
+			}
+		}
+		if(!flagContains){
+			fail("Error process model could not be found with business object(s)");
+		}
 		
 		try {
 			pm.delete();
@@ -101,22 +186,30 @@ public class SPATest {
 	}
 	
 	// ProcessModelInstance
+	//done
 	@Test
 	public void getProcessModelInstanceById() {
 		// ProcessInstance.getProcessInstance
-		ProcessInstance pm = createTestModelInstance(createTestModel());
+		ProcessModel pm = createTestModel();
+		ProcessInstance pi = createTestModelInstance(pm);
 		
 		try {
-			pm.store();
+			pi.store();
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Error in store!");
 		}
 		
-		fail("Not yet implemented!");
+		try {
+			ProcessInstance piNew = ProcessInstance.getProcessInstance(pm, pi.getId());
+			if(piNew == null) throw new Exception();
+		} catch (Exception e1) {
+			fail("Error in retrieving the Process Model by an ID");
+			e1.printStackTrace();
+		}
 		
 		try {
-			pm.delete();
+			pi.delete();
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Error in delete!");
@@ -126,10 +219,11 @@ public class SPATest {
 	@Test
 	public void getProcessInstanceIDsPID() {
 		// ProcessInstance.getProcessInstanceIDsPID
-		ProcessInstance pm = createTestModelInstance(createTestModel());
+		ProcessModel pm = createTestModel();
+		ProcessInstance pi = createTestModelInstance(pm);
 		
 		try {
-			pm.store();
+			pi.store();
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Error in store!");
@@ -138,7 +232,7 @@ public class SPATest {
 		fail("Not yet implemented!");
 		
 		try {
-			pm.delete();
+			pi.delete();
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Error in delete!");
@@ -168,6 +262,7 @@ public class SPATest {
 	}
 	
 	// BusinessObject
+	//done
 	@Test
 	public void getBusinessObject() {
 		// BusinessObject.getBusinessObject
@@ -179,21 +274,40 @@ public class SPATest {
 			e.printStackTrace();
 			fail("Error in store!");
 		}
+			ProcessModel newPM = null;
+		try {
+			newPM = ProcessModel.getProcess(pm.getId());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			fail("Error in retrieval");
+		}
 		
-		fail("Not yet implemented!");
+		for(BusinessObject boOld: pm.getAllBusisnessObjects()){
+			boolean containsFlag = false;
+			for(BusinessObject boNew: newPM.getAllBusisnessObjects()){
+				if (boNew.getId().equals(boOld.getId())){
+					containsFlag = true;
+				}
+			}
+			if(!containsFlag){
+				fail("Error with stroring/retrieving Business Objects");
+			}
+		}
+		
 		
 		try {
 			pm.delete();
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Error in delete!");
-		}
+		}	
 	}
 	
 	@Test
 	public void getBusinessObjectByKeywords() {
 		// BusinessObject.getBusinessObjectByKeywords
 		ProcessModel pm = createTestModel();
+		BusinessObject bo = new BusinessObject(pm);
 		
 		try {
 			pm.store();
@@ -216,6 +330,7 @@ public class SPATest {
 	public void getBusinessObjectsByProcessID() {
 		// BusinessObject.getBusinessObjectsByProcessID
 		ProcessModel pm = createTestModel();
+		BusinessObject bo = new BusinessObject(pm);
 		
 		try {
 			pm.store();
@@ -223,7 +338,7 @@ public class SPATest {
 			e.printStackTrace();
 			fail("Error in store!");
 		}
-		
+
 		fail("Not yet implemented!");
 		
 		try {
